0b28a16ccdbc8ef4412740c991fa7ceb23e07f04
Vladimir Balabanov <38167466+Grrr1337@users.noreply.github.com>
2023-12-03 22:35:19 +0200

    Added HotelBookingAPI

---
+++ REST/HotelBookingAPI/HotelBookingAPI/Models/HotelBooking.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBookingAPI.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public int RommNumber { get; set; }
+        public string? ClientName { get; set; }
+
+    }
+}
