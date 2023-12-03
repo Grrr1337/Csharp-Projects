ac482d383e8d07fb6885ef91f36401379a5e2955
Vladimir Balabanov <38167466+Grrr1337@users.noreply.github.com>
2023-12-03 13:37:22 +0200

    Updated SortAlgorithms

--- Sort-Algorithms/SortAlgorithms/SortAlgorithms/Program.cs
+++ Sort-Algorithms/SortAlgorithms/SortAlgorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace SortAlgorithms
 {
@@ -6,22 +7,99 @@ namespace SortAlgorithms
     {
         static void Main()
         {
-            int[] array = { 12, 11, 13, 5, 6 };
-
-            Console.WriteLine("Original Array:");
-            Algorithms.PrintArray(array);
-
+           
+            int size, minRange, maxRange;
+            size = 20000;
+            minRange = 0;
+            maxRange = 20001;
+            int[] array = GenericFunctions.GenerateIntArray(size, minRange, maxRange);
+            
+            // GenericFunctions.PrintArray("Original Array:", array);
+ 
             // Using Insertion Sort
-            Algorithms.InsertionSort(array);
-
-            Console.WriteLine("\nSorted Array using Insertion Sort:");
-            Algorithms.PrintArray(array);
+            int[] insertionSortedArray = Algorithms.InsertionSort(array);
+            Console.WriteLine($"Insertion Sort BenchMark: {string.Format("{0,12}", Algorithms.BenchmarkTime)} ms");
+            // GenericFunctions.PrintArray("Sorted Array using Insertion Sort:", insertionSortedArray);
 
             // Using Bubble Sort
-            Algorithms.BubbleSort(array);
+            int[] bubbleSortedArray = Algorithms.BubbleSort(array);
+            Console.WriteLine($"Bubble Sort BenchMark: {string.Format("{0,12}", Algorithms.BenchmarkTime)} ms");
+            // GenericFunctions.PrintArray("Sorted Array using Bubble Sort:", bubbleSortedArray);
 
-            Console.WriteLine("\nSorted Array using Bubble Sort:");
-            Algorithms.PrintArray(array);
+            // Using Selection Sort
+            int[] selectionSortedArray = Algorithms.SelectionSort(array);
+            Console.WriteLine($"Selection Sort BenchMark: {string.Format("{0,12}", Algorithms.BenchmarkTime)} ms");
+
+            // Using Quick Sort
+            // int[] quickSortedArray = Algorithms.QuickSort(array);
+            // Console.WriteLine($"Quick Sort BenchMark: {string.Format("{0,12}", Algorithms.BenchmarkTime)} ms");
+
+
+            // Check if all sorted arrays are the same
+            bool areSortedArraysSame = GenericFunctions.AreArraysSame(new List<int[]>
+            {
+                insertionSortedArray,
+                bubbleSortedArray,
+                selectionSortedArray,
+                // selectionSortedArray.Reverse().ToArray(), // <- to test if the `AreArraysSame` method work.
+                // Add more sorted arrays if needed
+            });
+
+            Console.WriteLine($"Are all sorted arrays the same? {areSortedArraysSame}");
+
+        }
+    }
+}
+
+
+namespace SortAlgorithms
+{
+    public class GenericFunctions
+    {
+        public static int[] GenerateIntArray(int size, int minRange, int maxRange)
+        {
+            Random random = new Random();
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(minRange, maxRange); // Generate a random integer between 0 and 100
+            }
+
+            return array;
+        }
+
+        public static void PrintArray(String header, int[] arr)
+        {
+            Console.WriteLine(header);
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+        }
+
+
+        public static bool AreArraysSame(List<int[]> arrays)
+        {
+            if (arrays == null || arrays.Count < 2)
+            {
+                // If the list is null or has less than 2 arrays, they are considered the same.
+                return true;
+            }
+
+            // Compare each element of the arrays
+            for (int i = 1; i < arrays.Count; i++)
+            {
+                if (!Enumerable.SequenceEqual(arrays[0], arrays[i]))
+                {
+                    // If any array is not equal to the first one, return false
+                    return false;
+                }
+            }
+
+            // All arrays are the same
+            return true;
         }
     }
 }
@@ -31,52 +109,109 @@ namespace SortAlgorithms
 
     public class Algorithms
     {
-     
-        public static void InsertionSort(int[] arr)
+        // private static long benchmarkTime; // Internal parameter to hold elapsed milliseconds
+
+        //public static long BenchmarkTime
+        //{
+        //    get { return benchmarkTime; }
+        //}
+
+        private static Stopwatch stopwatch = new Stopwatch();
+
+        public static TimeSpan BenchmarkTime
+        {
+            get { return stopwatch.Elapsed; }
+        }
+
+        public static int[] SelectionSort(int[] arr)
         {
             int n = arr.Length;
+            int[] sortedArray = arr.ToArray();
+
+            stopwatch.Restart();
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (sortedArray[j] < sortedArray[minIndex])
+                        minIndex = j;
+                }
+
+                // Swap sortedArray[i] and sortedArray[minIndex]
+                int temp = sortedArray[i];
+                sortedArray[i] = sortedArray[minIndex];
+                sortedArray[minIndex] = temp;
+            }
+
+            stopwatch.Stop();
+            return sortedArray;
+        }
+
+
+        public static int[] InsertionSort(int[] arr)
+        {
+            int n = arr.Length;
+            int[] sortedArray = arr.ToArray(); // Create a copy of the array
+
+            //Stopwatch stopwatch = new Stopwatch();
+            //stopwatch.Start();
+            stopwatch.Restart();
+
             for (int i = 1; i < n; ++i)
             {
-                int key = arr[i];
+                int key = sortedArray[i];
                 int j = i - 1;
 
-                // Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position
-                while (j >= 0 && arr[j] > key)
+                while (j >= 0 && sortedArray[j] > key)
                 {
-                    arr[j + 1] = arr[j];
+                    sortedArray[j + 1] = sortedArray[j];
                     j = j - 1;
                 }
 
-                arr[j + 1] = key;
+                sortedArray[j + 1] = key;
             }
+
+            //stopwatch.Stop();
+            //benchmarkTime = stopwatch.ElapsedMilliseconds;
+
+            stopwatch.Stop();
+
+            return sortedArray;
         }
 
-        public static void BubbleSort(int[] arr)
+        public static int[] BubbleSort(int[] arr)
         {
             int n = arr.Length;
+            int[] sortedArray = arr.ToArray(); // Create a copy of the array
+
+            //Stopwatch stopwatch = new Stopwatch();
+            //stopwatch.Start();
+
+            stopwatch.Restart();
+
             for (int i = 0; i < n - 1; i++)
             {
                 for (int j = 0; j < n - i - 1; j++)
                 {
-                    if (arr[j] > arr[j + 1])
+                    if (sortedArray[j] > sortedArray[j + 1])
                     {
-                        // Swap arr[j] and arr[j + 1]
-                        int temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
+                        // Swap sortedArray[j] and sortedArray[j + 1]
+                        int temp = sortedArray[j];
+                        sortedArray[j] = sortedArray[j + 1];
+                        sortedArray[j + 1] = temp;
                     }
                 }
             }
+
+            //stopwatch.Stop();
+            //benchmarkTime = stopwatch.ElapsedMilliseconds;
+
+            stopwatch.Stop();
+            return sortedArray;
         }
 
-        public static void PrintArray(int[] arr)
-        {
-            foreach (var item in arr)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-        }
     }
 
 }
