75069905e4550a8cfe97614268842a7587e46cc6
Vladimir Balabanov <38167466+Grrr1337@users.noreply.github.com>
2023-12-03 15:23:07 +0200

    Updated String Manipulations

--- String-Manipulations/StringManipulations/StringManipulations/Program.cs
+++ String-Manipulations/StringManipulations/StringManipulations/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Collections.Generic;
 
 namespace StringManipulations
 {
@@ -13,18 +14,30 @@ namespace StringManipulations
             // Concatenation
             Console.WriteLine($"\nConcatenation result: {BasicStringUtils.ConcatenateStrings(str1, str2)}");
 
-            string original = "Hello World";
+            string original = "Hello Vlado";
 
             // Substring
             Console.WriteLine($"\nSubstring result: {BasicStringUtils.ExtractSubstring(original, 6)}");
+
+            // Contains
+            Console.WriteLine($"\nContains result: {BasicStringUtils.IsStringContained("Vladimir writes code almost every day", "every")}");
 
             // Length
             Console.WriteLine($"\nLength of the string: {BasicStringUtils.GetStringLenght(original)}");
 
             // Replace
-            string sentence = "C# is fun!";
-            string newSentence = BasicStringUtils.ReplaceSubstring(sentence, "fun", "awesome");
+            string sentence = "Vlado likes to party!";
+            string newSentence = BasicStringUtils.ReplaceSubstring(sentence, "party", "code");
             Console.WriteLine($"\nReplace result: {newSentence}");
+
+            Console.WriteLine("\nChange case string results:");
+            // ToLower, ToUpper, ToCamelCase
+            string mixedCase = "MiXeD CaSe t3st";
+            Console.WriteLine($"ToLower: {BasicStringUtils.ConvertToLower(mixedCase)}");
+            Console.WriteLine($"ToUpper: {BasicStringUtils.ConvertToUpper(mixedCase)}");
+            Console.WriteLine($"ToCamel: {BasicStringUtils.ToCamelCase(mixedCase)}" );
+            Console.WriteLine($"ToPascal: {BasicStringUtils.ToPascalCase(mixedCase)}");
+
 
             // Split
             Console.WriteLine("\nSplit string result:");
@@ -35,13 +48,30 @@ namespace StringManipulations
                 Console.WriteLine(name);
             }
 
-            Console.WriteLine("\nChange case string results:");
-            // ToLower, ToUpper, ToCamelCase
-            string mixedCase = "MiXeD CaSe t3st";
-            Console.WriteLine($"ToLower: {BasicStringUtils.ConvertToLower(mixedCase)}");
-            Console.WriteLine($"ToUpper: {BasicStringUtils.ConvertToUpper(mixedCase)}");
-            Console.WriteLine($"ToCamel: {BasicStringUtils.ToCamelCase(mixedCase)}" );
-            Console.WriteLine($"ToPascal: {BasicStringUtils.ToPascalCase(mixedCase)}");
+            Console.WriteLine("\nSplit string to chars result:");
+            foreach (char character in BasicStringUtils.SplitStringToChars("Hello World"))
+            {
+                Console.WriteLine(character);
+            }
+
+            Console.WriteLine("\nSplit string to words result:");
+            foreach (string word in BasicStringUtils.SplitStringSentenceToWords("The Quick Brown Fox"))
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine("\nSplit string by delimeter: ");
+            foreach (string part in BasicStringUtils.SplitStringByDelimeter(new string("apple,orange,banana,apple,grape"), new String("apple,")))
+            {
+                Console.WriteLine(part);
+            }
+
+            // Join array or List into a string
+            Console.WriteLine($"{BasicStringUtils.JoinString(new string[] { "This", "is", "a", "join", "Array", "Test" }, "_")}");
+            Console.WriteLine($"{BasicStringUtils.JoinString(new List<string> { "This", "is", "a", "join", "List", "Test" }, "-")}");
+
+
+
         }
     }
 }
@@ -60,6 +90,12 @@ public class BasicStringUtils
         return original.Substring(startIndex);
     }
 
+    public static bool IsStringContained(string mainString, string searchString)
+    {
+        bool containsString = mainString.Contains(searchString);
+        return containsString;
+    }
+
     public static int GetStringLenght(string text)
     {
         return text.Length;
@@ -68,11 +104,6 @@ public class BasicStringUtils
     public static string ReplaceSubstring(string input, string oldSubstring, string newSubstring)
     {
         return input.Replace(oldSubstring, newSubstring);
-    }
-
-    public static string[] SplitString(string input, char separator)
-    {
-        return input.Split(separator);
     }
 
     public static string ConvertToLower(string input)
@@ -125,4 +156,39 @@ public class BasicStringUtils
         // Concatenate the words back into a single string
         return string.Join("", words);
     }
+
+    public static string[] SplitString(string input, char separator)
+    {
+        return input.Split(separator);
+    }
+
+    public static char[] SplitStringToChars(string inputString)
+    {
+        // Split the string into characters
+        char[] characters = inputString.ToCharArray();
+        return characters;
+    }
+
+    public static string[] SplitStringSentenceToWords(string inputString)
+    {
+        string[] words = inputString.Split();
+        return words;
+    }
+
+    public static string[] SplitStringByDelimeter(string inputString, string delimeter)
+    {
+        string[] result = inputString.Split(new string[] { delimeter }, StringSplitOptions.RemoveEmptyEntries);
+        return result;
+    }
+
+    public static string JoinString(string[] stringArray, string delimeter)
+    {
+       return string.Join(delimeter, stringArray);
+    }
+
+    public static string JoinString(List<string> stringList, string delimeter)
+    {
+        return string.Join(delimeter, stringList);
+    }
+
 }
