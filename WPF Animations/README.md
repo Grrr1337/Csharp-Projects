d271aa028510b0e150fe3c63e31b361bfd7d8a18
Vladimir Balabanov <38167466+Grrr1337@users.noreply.github.com>
2023-12-21 14:55:12 +0200

    Created 'WPF Animations'

---
+++ WPF Animations/README.md
@@ -0,0 +1,86 @@
+# WPF Animations
+
+This project is a training one that delves into the realm of animations in WPF. The project provides a basic exploration of animations, both from the XAML markup perspective and through code-behind implementation. Whether you're interested in declarative animations in XAML or dynamic animations in C#, this project covers it all.
+
+## Preview
+![WPF Animations](WPF%20Animations%20Demo.gif)
+
+
+## Key Concepts Learned
+
+### 1. Declarative XAML Animations
+- **Button Hover Effects:**
+  - Implements smooth and visually appealing hover effects using XAML triggers.
+  - Utilizes `DoubleAnimation` to dynamically adjust button size.
+  - Demonstrates `ColorAnimation` for seamless background color transitions.
+
+### 2. Dynamic Code-Behind Animations
+- **Interactive Button Animations:**
+  - Responds to mouse enter and leave events to trigger dynamic animations in code-behind.
+  - Utilizes `DoubleAnimation` and `ColorAnimation` to create engaging button interactions.
+
+### 3. Window Dragging with Animation
+- **Draggable Window Enhancement:**
+  - Implements interactive window dragging with a smooth animation effect.
+  - Enhances user experience by providing an animated window movement.
+
+### 4. Stylish Button Customization
+- **Button Styling Techniques:**
+  - Showcases styling approaches for buttons in both XAML and code-behind.
+  - Utilizes templates to customize button appearance while maintaining animation responsiveness.
+
+### 5. Event-Driven Animation Handling
+- **Mouse Events for Animation Control:**
+  - Efficiently handles mouse events (e.g., `MouseEnter` and `MouseLeave`) to control animations.
+  - Achieves a cohesive user experience through event-driven animation management.
+
+
+## Getting Started
+
+### Prerequisites
+
+- **Visual Studio:** This project is developed using Visual Studio, and it is recommended to have Visual Studio installed for a seamless development experience.
+  
+- **.NET Framework 4.8:** Ensure that you have the .NET Framework 4.8 installed on your development machine. You can download it from the [official Microsoft site](https://dotnet.microsoft.com/download/dotnet-framework/net48).
+
+### Clone the Repository
+
+1. Clone this repository to your local machine using the following command:
+
+    ```bash
+    git clone https://github.com/Grrr1337/Csharp-Projects.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd WPF-Drawing
+    ```
+
+### Open in Visual Studio
+
+1. Open Visual Studio.
+
+2. Select **File > Open > Project/Solution** and navigate to the cloned project directory.
+
+3. Choose the `WPF Animations.sln` solution file and click **Open**.
+
+### Build and Run
+
+1. Build the solution by pressing `Ctrl + Shift + B` or selecting **Build > Build Solution** from the menu.
+
+2. Run the project by pressing `F5` or selecting **Debug > Start Debugging** from the menu.
+
+3. The application window will appear, showcasing the custom controls in action.
+
+## Author
+Vladimir Balabanov ( **Grrr1337** )
+
+## License
+This project is licensed under the MIT License.
+
+
+## Contributors
+ 
+ This project is based on this tutorial: [How to Create Engaging Animations in WPF using Storyboards and Code-Behind](https://www.youtube.com/watch?v=qK90unxfrXw)
+
---
+++ WPF Animations/WPF Animations/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+ 
+
+namespace WPF_Animations
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            this.Title = "WPF Animations";
+            this.ResizeMode = System.Windows.ResizeMode.NoResize;
+            this.Focusable = true;
+            this.Loaded += MainWindow_Loaded;
+            this.MouseLeftButtonDown += MainWindow_MouseLeftButtonDown;
+        }// public MainWindow()
+
+        private void MainWindow_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                // Begin dragging the window
+                DragMove();
+            }
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            btnClose.Click += (_s, _e) =>
+            {
+                Close();
+            };
+            lblAuthor.MouseEnter += (_s, _e) => {
+                lblAuthor.Content = "Vladimir Balabanov";
+                lblAuthor.Foreground = System.Windows.Media.Brushes.Red;
+            };
+
+            lblAuthor.MouseLeave += (_s, _e) => {
+                lblAuthor.Content = "Author";
+                lblAuthor.Foreground = System.Windows.Media.Brushes.White;
+            };
+
+            foreach (var child in btnSet2.Children)
+            {
+                if (child is Button button)
+                {
+                    button.MouseEnter += (_s, _e) =>
+                    {
+                        var btn = _s as Button;
+                        var widthAnimation = new DoubleAnimation() { To = 130, Duration = TimeSpan.FromSeconds(0.3) };
+                        var heightAnimation = new DoubleAnimation() { To = 150, Duration = TimeSpan.FromSeconds(0.3) };
+
+                        var colorAnimation = new ColorAnimation
+                        {
+                            To = Colors.MediumSlateBlue,
+                            Duration = TimeSpan.FromSeconds(0.3)
+                        };
+
+                        var originalBackgroundBrush = btn.Background as SolidColorBrush;
+
+                        if (originalBackgroundBrush != null)
+                        {
+                            // Create a new SolidColorBrush instance with the same color
+                            var newBackgroundBrush = new SolidColorBrush(originalBackgroundBrush.Color);
+
+                            // Apply the animation to the new SolidColorBrush
+                            newBackgroundBrush.BeginAnimation(SolidColorBrush.ColorProperty, colorAnimation);
+
+                            // Set the new SolidColorBrush as the button's background
+                            btn.Background = newBackgroundBrush;
+                        }
+
+                        btn.BeginAnimation(Button.WidthProperty, widthAnimation);
+                        btn.BeginAnimation(Button.HeightProperty, heightAnimation);
+                    }; // button.MouseEnter
+
+
+                    button.MouseLeave += (_s, _e) =>
+                    {
+                        var btn = _s as Button;
+                        var WidthAnimation = new DoubleAnimation() { To = 100, Duration = TimeSpan.FromSeconds(0.3) };
+                        var HeightAnimation = new DoubleAnimation() { To = 120, Duration = TimeSpan.FromSeconds(0.3) };
+
+
+                        var colorAnimation = new ColorAnimation
+                        {
+                            To = Colors.Transparent,
+                            Duration = TimeSpan.FromSeconds(0.3)
+                        };
+
+                        var backgroundBrush = btn.Background as SolidColorBrush;
+
+                        if (backgroundBrush != null)
+                        {
+                            backgroundBrush.BeginAnimation(SolidColorBrush.ColorProperty, colorAnimation);
+                        }
+
+                        btn.BeginAnimation(Button.WidthProperty, WidthAnimation);
+                        btn.BeginAnimation(Button.HeightProperty, HeightAnimation);
+                    }; // button.MouseLeave
+                }
+            }// foreach
+        }// private void MainWindow_Loaded
+
+
+
+
+    }// class
+}// namespace
